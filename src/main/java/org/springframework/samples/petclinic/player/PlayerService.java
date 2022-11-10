0808640bddbefdf7398b75ac3d4753f52d4ee037
AMC112002 <alejandromateocapilla@gmail.com>
2022-11-10 15:53:46 +0100

    Creación de player

---
+++ src/main/java/org/springframework/samples/petclinic/player/PlayerService.java
@@ -0,0 +1,37 @@
+package org.springframework.samples.petclinic.player;
+
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;	
+
+
+@Service
+public class PlayerService {
+	
+PlayerRepository repo;
+	
+	@Autowired 
+	PlayerService(PlayerRepository repo) {
+		this.repo = repo;
+	}
+	
+	List<Player> getPlayers() {
+		return repo.findAll();
+	}
+	
+	public Player getPlayerById(Integer id) {
+    	return repo.findById(id).get();
+    }
+    
+    public void deletePlayerById(Integer id) {
+    	repo.deleteById(id);
+    }
+    
+    public void save(Player player) {
+    	repo.save(player);
+    }
+
+}
