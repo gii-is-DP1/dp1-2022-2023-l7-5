356640d51af88c29738710963b6dee8a1b774f12
angmunpri <angmunpri@alum.us.es>
2022-11-13 16:49:03 +0100

    commit

---
+++ src/test/java/org/springframework/samples/petclinic/player/PlayerServiceTest.java
@@ -0,0 +1,23 @@
+package org.springframework.samples.petclinic.player;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.petclinic.user.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class PlayerServiceTest {
+	
+	@Autowired 
+	protected PlayerService serv;
+	
+	
+
+}
