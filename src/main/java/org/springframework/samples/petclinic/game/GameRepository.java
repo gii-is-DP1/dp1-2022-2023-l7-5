e4f2b7c6671ef416258cb5de37723b59854a4cb1
Bogdan Stefan <sbogdanm26@hotmail.com>
2022-11-14 20:00:00 +0100

    Fixing the GameForm

--- src/main/java/org/springframework/samples/petclinic/game/GameRepository.java
+++ src/main/java/org/springframework/samples/petclinic/game/GameRepository.java
@@ -2,10 +2,15 @@ package org.springframework.samples.petclinic.game;
 
 import java.util.List;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameRepository extends CrudRepository<Game, Integer> {
 	List<Game> findAll();
+	
+	@Query("SELECT mode FROM Mode mode ORDER BY mode.name")
+	List<Mode> findModes() throws DataAccessException;
 }
