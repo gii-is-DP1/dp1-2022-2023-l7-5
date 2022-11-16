06ed736f9bbb050f770daca9475c6d5852fa5bff
AMC112002 <alejandromateocapilla@gmail.com>
2022-11-16 13:13:25 +0100

    Commit obligado :(

--- src/test/java/org/springframework/samples/petclinic/scoreboard/ScoreBoardServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/scoreboard/ScoreBoardServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
