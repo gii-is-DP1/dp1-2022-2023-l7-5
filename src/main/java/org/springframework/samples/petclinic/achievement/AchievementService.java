fb7d20785a692354038f4d955e3b74a9a3fa7225
angmunpri <angmunpri@alum.us.es>
2022-12-13 18:56:57 +0100

    Imagenes Candado y test de Profile y Achievement

--- src/main/java/org/springframework/samples/petclinic/achievement/AchievementService.java
+++ src/main/java/org/springframework/samples/petclinic/achievement/AchievementService.java
@@ -47,41 +47,6 @@ public class AchievementService {
 
 	}
 
-	public void createAchievements() {
-
-		Achievement a = new Achievement();
-		a.setBadgeImage(
-				"https://media.istockphoto.com/illustrations/happy-baby-bee-illustration-id92717135?k=6&m=92717135&s=170667a&w=0&h=rT8nF4gaMA1bjExYyX4jTlTERmNtuVhVihHE5bmavIc=");
-		a.setName("Baby Bee");
-		a.setDescription("Enter your first game");
-		a.setThreshold(2.);
-		repo.save(a);
-		Achievement a2 = new Achievement();
-		a2.setBadgeImage("abejorro.jpg");
-		a2.setName("BumbleBee");
-		a2.setDescription("Play 5 games");
-		a2.setThreshold(2.);
-		repo.save(a2);
-		Achievement a3 = new Achievement();
-		a3.setBadgeImage("shorturl.at/tDX08");
-		a3.setName("Queen Bee");
-		a3.setDescription("Play 10 games");
-		a3.setThreshold(2.);
-		repo.save(a3);
-		Achievement a4 = new Achievement();
-		a4.setBadgeImage("winner.jpg");
-		a4.setName("Queen of the Hive");
-		a4.setDescription("Win your first game");
-		a4.setThreshold(2.);
-		repo.save(a4);
-		Achievement a5 = new Achievement();
-		a5.setBadgeImage("ladron.jpg");
-		a5.setName("Winnie The Pooh");
-		a5.setDescription("Steal your first tile");
-		a5.setThreshold(2.);
-		repo.save(a5);
-
-	}
 
 	public void giveFirstAchieve(Profile p) {
 
