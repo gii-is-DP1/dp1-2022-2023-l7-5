ec0ea5deac20e4d1916373cee7434c2a2502db7a
Manuel Vázquez <manvazmar3@alum.us.es>
2023-01-11 20:13:52 +0100

    Merge remote-tracking branch 'origin/master' into manvazmar3.4

--- src/test/java/org/springframework/samples/petclinic/user/UserControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/user/UserControllerTest.java
@@ -19,7 +19,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.samples.petclinic.achievement.AchievementService;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.profile.ProfileService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,10 +29,22 @@ import org.springframework.test.web.servlet.MockMvc;
 @WebMvcTest(controllers = UserController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class UserControllerTest {
 
-	private static final String TEST_ID = "ejemplo";
+	private static final String TEST_ID = "manuel";
 	
 	@Autowired
+	private UserController userController;
+	
+	@MockBean
 	private UserService userService;
+	
+	@MockBean
+	private AuthoritiesService authoritiesService;
+	
+	@MockBean
+	private AchievementService achievementService;
+	
+	@MockBean
+	private ProfileService profileService;
 	
 	@Autowired
 	private MockMvc mockMvc;
@@ -38,32 +52,92 @@ public class UserControllerTest {
 	@BeforeEach
 	void setup() {
 		User user = new User();
-		user.setUsername("ejemplo");
+		user.setUsername("manuel");
 		user.setEmail("manuel.ejemplo@gmail.com");
 		user.setPassword("password");
-		user.setEnabled(true);
-		
+//		user.setEnabled(true);
+//		
 //		Authorities auth = new Authorities();
 //		auth.setUser(user);
-//		auth.setAuthority("player");
+//		auth.setAuthority("admin");
 		
 		given(this.userService.findAllUsers()).willReturn(Lists.newArrayList(user));
-		given(this.userService.findUser(TEST_ID).get()).willReturn(new User());
+//		given(this.userService.findUser(TEST_ID).get()).willReturn(user);
 	}
 	
 //	@WithMockUser(value = "spring")
 //	@Test
 //	void testShowPlayers() throws Exception {
-//		mockMvc.perform(get("/players/PlayersListing")).andExpect(status().isOk()).andExpect(model().attributeExists("players"))
-//		.andExpect(view().name("/players/PlayersListing"));
+//		mockMvc.perform(get("/users/page/{page}", "2")).andExpect(status().isOk()).andExpect(model().attributeExists("page"))
+//		.andExpect(view().name("/users/page/2"));
 //	}
 	
 	@WithMockUser(value = "spring")
 	@Test
 	void testInitCreationUser() throws Exception {
 		
-		mockMvc.perform(get("users/new")).andExpect(status().isOk())
-			.andExpect(view().name("users/createPlayerForm")).andExpect(model().attributeExists("users"));
+		mockMvc.perform(get("/users/new")).andExpect(status().isOk())
+			.andExpect(view().name("users/createPlayerForm")).andExpect(model().attributeExists("user"));
 		
+	}
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testProcessCreationUserSucess() throws Exception {
+		
+		mockMvc.perform(post("/users/new").with(csrf()).param("username", "Capi")
+		.param("email", "test@gmail.com").param("password", "contrasena").param("enabled", "true"))
+		.andExpect(status().is3xxRedirection())
+		.andExpect(view().name("redirect:/"));
+		
+	}
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testProcessCreationUsersHasErrors() throws Exception {
+		
+		mockMvc.perform(post("/users/new").with(csrf())
+		.param("username", "ejemplo").param("email", "gmailcom").param("password", "contrasena").param("enabled", "true"))
+		.andExpect(view().name("exception"));
+		
+	}
+	
+//	@WithMockUser(value = "spring")
+//	@Test
+//	void testInitUpdateUser() throws Exception {
+//		
+//		mockMvc.perform(get("/player/{username}/edit", TEST_ID)).andExpect(status().isOk())
+//			.andExpect(view().name("/player/ejemplo/edit")).andExpect(model().attributeExists("user"));
+//		
+//	}
+//	
+//	@WithMockUser(value = "spring")
+//	@Test
+//	void testProcessUpdateUserSuccess() throws Exception {
+//		
+//		mockMvc.perform(post("/player/{username}/edit", TEST_ID).with(csrf()).param("name", "Capi")
+//		.param("email", "test@gmail.com").param("password", "contrasena").param("enabled", "true"))
+//		.andExpect(status().isOk())
+//		.andExpect(view().name("player/PlayersListing"));
+//		
+//	}
+//	
+//	@WithMockUser(value = "spring")
+//	@Test
+//	void testProcessUpdateUserHasErrors() throws Exception {
+//		
+//		mockMvc.perform(post("/player/{username}/edit", TEST_ID).with(csrf())
+//		.param("email", "test@gmail.com").param("password", "contrasena").param("enabled", "true"))
+//		.andExpect(model().attributeHasErrors("user"))
+//		.andExpect(status().isOk()).andExpect(view().name("users/createPlayerForm"));
+//		
+//	}
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testShowUser() throws Exception {
+		mockMvc.perform(get("/player/{username}", TEST_ID)).andExpect(status().isOk())
+		.andExpect(model().attributeExists("user"))
+		.andExpect(view().name("player/manuel"));
 	}
 }
