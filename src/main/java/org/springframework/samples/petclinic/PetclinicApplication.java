77605c5cc5c6147bfa9d21294b9d4cd682358acc
Bogdan Ștefan <bogste@alum.us.es>
2023-01-12 22:52:16 +0100

    Update PetclinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication()
-@EnableJpaAuditing
+
 public class PetclinicApplication {
 
 	public static void main(String[] args) {
